d5d261b6583943503a5d00015774840069bbfe85
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-07-29 19:38:16 -0300

    feat: :tada: first commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  white: "#FFFFFF",
+
+  "gray-100": "#F7FAFC",
+  "gray-300": "#E2E8F0",
+  "gray-400": "#CBD5E0",
+  "gray-500": "#A0AEC0",
+  "gray-600": "#718096",
+  "gray-700": "#4A5568",
+  "gray-800": "#2D3748",
+  "gray-900": "#1A202C",
+
+  "blue-300": "#90CDF4",
+  "blue-500": "#4299E1",
+  "blue-700": "#2B6CB0",
+
+  "green-300": "#9AE6B4",
+  "green-500": "#48BB78",
+  "green-700": "#2F855A",
+
+  "red-300": "#FEB2B2",
+  "red-500": "#F56565",
+  "red-700": "#C53030",
+} as const;
