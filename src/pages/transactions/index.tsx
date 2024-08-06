a994463fa4d1f4aec701c379117005cc98cbeae0
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-08-06 20:06:17 -0300

    feat: :sparkles: add transactions table

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,12 +1,41 @@
 import { Header } from "../../components/header";
 import { Summary } from "../../components/summary";
 
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+
 export function Transactions() {
   return (
     <div>
       <Header />
-
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>6/8/2024</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight>- R$ 50,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>1/8/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
