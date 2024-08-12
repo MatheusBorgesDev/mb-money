ad43fca67948e048e95da784600c30b3922c84dc
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-08-12 16:01:26 -0300

    feat: :sparkles: fetch transactions data to show in home page table

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { Header } from "../../components/header";
 import { Summary } from "../../components/summary";
 import { SearchForm } from "./components/searchForm";
@@ -8,7 +10,29 @@ import {
   TransactionsTable,
 } from "./styles";
 
+interface Transaction {
+  id: number;
+  from: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
   return (
     <div>
       <Header />
@@ -19,23 +43,20 @@ export function Transactions() {
 
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>6/8/2024</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Hambúrguer</td>
-              <td>
-                <PriceHighlight>- R$ 50,00</PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>1/8/2024</td>
-            </tr>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="45%">{transaction.from}</td>
+                <td>
+                  <PriceHighlight
+                    variant={
+                      transaction.type === "income" ? "income" : undefined
+                    }
+                  >{`R$ ${transaction.price}`}</PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
